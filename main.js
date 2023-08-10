5e24d5a39bc6a9b8d064a241c3f5445d723ff241
scratchis <103792439+scratchis@users.noreply.github.com>
2023-08-10 19:35:50 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -19,6 +19,15 @@ function setup(){
 }
 function draw(){
     image(video,0,0,600,500);
+    fill("#FF0000");
+    stroke("#FF0000");
+    if(scoreLeftWrist>0.2){
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY=Number(leftWristY);
+    remove_decimals=floor(InNumberleftWristY);
+    volume=remove_decimals/500;
+    document.getElementById("volume").innerHTML="Volume = "+volume;
+    song.setVolume(volume);
 }
 function play(){
     song.play();
@@ -30,6 +39,9 @@ function modalLoaded(){
 }
 function gotPoses(results){
     if(results.lenth>0){
+        console.log(results);
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = "+scoreLeftWrist);
         console.log(results);
         leftWristX=results[0].pose.leftWrist.x
         leftWristY=results[0].pose.leftWrist.y
